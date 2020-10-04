0a5af054e2518bf74b3afaeb6510c28b8c7bcadf
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2020-10-03 22:06:41 -0700

    Update TileCollection docs, fix tests/Makefile

--- hw3/scrabble/tile_collection.h
+++ hw3/scrabble/tile_collection.h
@@ -8,24 +8,66 @@
 
 typedef std::map<TileKind, size_t> TileMap;
 
+/*
+ Special data structure used for storing tiles.  Allows you to store individual
+ instances of a tile and keeps track of how many are currently in the collection.
+*/
 class TileCollection {
 public:
     class const_iterator;
 
+    /*
+     Adds one of the given tile to the collection.
+     */
     void add_tile(TileKind kind);
+
+    /*
+     Adds n of the given tile to the collection.
+     */
     void add_tiles(TileKind kind, size_t n);
 
+    /*
+     Removes one of the given tile from the collection.
+     If this tile does not exist in the collection, an std::out_of_range exception is thrown.
+     */
     void remove_tile(TileKind kind);
+
+    /*
+     Removes n of the given tile from the collection.
+     If this tile does not exist in the collection, or if not enough exist to satisfy the request,
+     an std::out_of_range exception is thrown.
+     */
     void remove_tiles(TileKind kind, size_t n);
 
+    /*
+     Find the TileKind in this collection matching a given letter.
+     Throws std::out_of_range if no tile with this letter exists in the collection.
+     */
     TileKind lookup_tile(char letter) const;
 
+    /*
+     Get the total number of all tiles in this collection.
+     */
     size_t count_tiles() const;
+
+    /*
+     Get the total number of this kind of tile that exist in the collection.
+     */
     size_t count_tiles(TileKind kind) const;
 
+    /*
+     Get the sum of all point values of all tiles in the collection
+     */
     unsigned int total_points() const;
 
+    /*
+     Get an iterator to the first element
+     */
     const_iterator cbegin() const;
+
+    /*
+     Get an iterator past the last element
+     */
     const_iterator cend() const;
 
     class const_iterator
